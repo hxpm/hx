277eb6ddfb3afe9b5f9d703f3f74b59f0e6cf887
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-04-30 16:43:14 -0400

    chore: created wilwind.config.js

---
+++ assets/tailwind.config.js
@@ -0,0 +1,14 @@
+// See the Tailwind configuration guide for advanced usage
+// https://tailwindcss.com/docs/configuration
+
+let plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
+
+  plugins: [],
+
+  theme: {
+    extend: {},
+  },
+};

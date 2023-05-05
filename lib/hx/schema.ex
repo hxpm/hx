348c1035f15a9b87f5ebfc0ae35072ccf269925c
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-05-05 11:16:41 -0400

    chore: set table/schema timestamps type to :utc_datetime

--- lib/hx/schema.ex
+++ lib/hx/schema.ex
@@ -1,17 +1,17 @@
 defmodule Hx.Schema do
   @moduledoc """
-  Used to extend an Ecto schema with defaults sensible to Hx.
+  Defines an Ecto schema.
 
-  `Hx.Schema` sets the default timestamps type to `:utc_datetime_usec`. Use this
-  in place of `Ecto.Schema` for all repository backed schemas. Continue using
-  `Ecto.Schema` for embedded schemas.
+  Sets the default timestamps type to `:utc_datetime`. Use this in place of
+  `Ecto.Schema` for all repository backed schemas. Continue using `Ecto.Schema`
+  for embedded schemas.
   """
 
   defmacro __using__(_opts) do
     quote do
       use Ecto.Schema
 
-      @timestamps_opts [type: :utc_datetime_usec]
+      @timestamps_opts [type: :utc_datetime]
     end
   end
 end

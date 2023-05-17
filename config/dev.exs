2ee7be32eea41d3a3c98d70c863ac3378c462f67
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-05-16 22:29:03 -0400

    chore: specify TailwindCSS config path

--- config/dev.exs
+++ config/dev.exs
@@ -9,6 +9,8 @@ config :hx, HxWeb.Endpoint,
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
     npx: [
       "tailwindcss",
+      "-c",
+      "assets/tailwind.config.js",
       "-i",
       "assets/css/hx.css",
       "-o",

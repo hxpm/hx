b3e49cca9d80a35d01207903ff3001a074c83b51
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-05-28 21:39:12 -0400

    chore: added LiveView formatter plugin (#60)

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [Phoenix.LiveView.HTMLFormatter]
 ]

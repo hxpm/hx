7224f6790641b4294f464d13d15319d62b3ac4d7
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-06-04 19:46:51 -0400

    chore: added type and value attributes to the form input component

--- test/hx_web/components/form/input_test.exs
+++ test/hx_web/components/form/input_test.exs
@@ -1,4 +1,4 @@
-defmodule HxWeb.Component.InputTest do
+defmodule HxWeb.Components.InputTest do
   use HxWeb.ConnCase, async: true
 
   use Phoenix.Component
@@ -12,12 +12,40 @@ defmodule HxWeb.Component.InputTest do
 
     template = ~H"<.input />"
 
-    component =
+    html_tree =
       template
       |> rendered_to_string()
       |> Floki.parse_document!()
       |> Floki.find("input")
 
-    assert [{"input", _, _}] = component
+    assert [{"input", _, _}] = html_tree
+  end
+
+  test "supports type attribute" do
+    assigns = %{type: "text"}
+
+    template = ~H"<.input type={@type} />"
+
+    [type] =
+      template
+      |> rendered_to_string()
+      |> Floki.parse_document!()
+      |> Floki.attribute("input", "type")
+
+    assert type == assigns[:type]
+  end
+
+  test "supports value attribute" do
+    assigns = %{value: "👍"}
+
+    template = ~H"<.input value={@value} />"
+
+    [value] =
+      template
+      |> rendered_to_string()
+      |> Floki.parse_document!()
+      |> Floki.attribute("input", "value")
+
+    assert value == assigns[:value]
   end
 end

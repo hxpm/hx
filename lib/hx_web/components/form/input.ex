de59f3233e3fc6676ab7af46b3a838dcf6f9f2dc
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-06-04 22:23:03 -0400

    chore: added support for displaying an error to HxWeb.Components.Input

--- lib/hx_web/components/form/input.ex
+++ lib/hx_web/components/form/input.ex
@@ -15,6 +15,8 @@ defmodule HxWeb.Components.Input do
 
   attr :value, :string, default: nil
 
+  slot :error
+
   def input(%{field: %Phoenix.HTML.FormField{} = field} = assigns) do
     assigns =
       assigns
@@ -26,18 +28,34 @@ defmodule HxWeb.Components.Input do
   end
 
   def input(assigns) do
+    error_id = assigns[:id] && assigns[:id] <> "_error"
+
+    has_error = assigns[:error] != []
+
     value = Phoenix.HTML.Form.normalize_value(assigns[:type], assigns[:value])
 
-    assigns = assign(assigns, :value, value)
+    assigns =
+      assigns
+      |> assign(:error_id, error_id)
+      |> assign(:has_error, has_error)
+      |> assign(:value, value)
 
     ~H"""
     <input
+      aria-describedby={@has_error && @error_id}
+      aria-invalid={@has_error}
       class={[
         "block border-0 ring-1 ring-inset ring-gray-300 rounded-md py-1.5 shadow-sm text-gray-900 w-full",
         "disabled:bg-gray-50 disabled:cursor-not-allowed disabled:ring-gray-200 disabled:text-gray-500",
         "focus:ring-2 focus:ring-indigo-600 focus:ring-inset",
         "placeholder:text-gray-400",
-        "sm:leading-6 sm:text-sm"
+        "sm:leading-6 sm:text-sm",
+        @error != [] &&
+          [
+            "ring-red-300 text-red-900",
+            "focus:ring-red-500",
+            "placeholder:text-red-300"
+          ]
       ]}
       disabled={@disabled}
       id={@id}
@@ -46,6 +64,9 @@ defmodule HxWeb.Components.Input do
       value={@value}
       {@rest}
     />
+    <p :for={error <- @error} class="mt-2 text-red-600 text-sm" id={@error_id}>
+      <%= render_slot(error) %>
+    </p>
     """
   end
 end

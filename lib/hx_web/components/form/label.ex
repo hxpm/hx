b21d9446f335ba40de5d56d4719249c27e8de67d
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-06-04 21:01:58 -0400

    chore: added field attribute to HxWeb.Components.Label

--- lib/hx_web/components/form/label.ex
+++ lib/hx_web/components/form/label.ex
@@ -1,13 +1,26 @@
 defmodule HxWeb.Components.Label do
   use Phoenix.Component
 
-  attr :rest, :global, include: ["for"]
+  attr :field, Phoenix.HTML.FormField
+
+  attr :for, :string, default: nil
+
+  attr :rest, :global
 
   slot :inner_block, required: true
 
+  def label(%{field: %Phoenix.HTML.FormField{} = field} = assigns) do
+    assigns =
+      assigns
+      |> assign(:field, nil)
+      |> assign(:for, field.id)
+
+    label(assigns)
+  end
+
   def label(assigns) do
     ~H"""
-    <label class="block font-medium leading-6 text-gray-900 text-sm" {@rest}>
+    <label class="block font-medium leading-6 text-gray-900 text-sm" for={@for} {@rest}>
       <%= render_slot(@inner_block) %>
     </label>
     """
--- test/hx_web/components/form/label_test.exs
+++ test/hx_web/components/form/label_test.exs
@@ -23,4 +23,37 @@ defmodule HxWeb.Components.LabelTest do
     assert [{"label", _, _}] = html_tree
     assert inner_block =~ assigns[:inner_block]
   end
+
+  test "when the field attribute is present the for attribute is automatically added" do
+    form =
+      %Hx.Identity.User{}
+      |> Ecto.Changeset.cast(%{}, [])
+      |> to_form()
+
+    assigns = %{form: form, inner_block: "👍"}
+
+    template = ~H"<.label field={@form[:email]}><%= @inner_block %></.label>"
+
+    [label_for] =
+      template
+      |> rendered_to_string()
+      |> Floki.parse_document!()
+      |> Floki.attribute("label", "for")
+
+    assert label_for == assigns[:form][:email].id
+  end
+
+  test "supports for attribute" do
+    assigns = %{for: "🙋", inner_block: "👍"}
+
+    template = ~H"<.label for={@for}><%= @inner_block %></.label>"
+
+    [label_for] =
+      template
+      |> rendered_to_string()
+      |> Floki.parse_document!()
+      |> Floki.attribute("label", "for")
+
+    assert label_for == assigns[:for]
+  end
 end

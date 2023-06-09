3a2fd2e5b80e99513731743576127f719255afa5
Anthony Smith <420061+anthonator@users.noreply.github.com>
2023-06-08 20:01:07 -0500

    fix: type

--- lib/hx_web/gettext.ex
+++ lib/hx_web/gettext.ex
@@ -20,7 +20,7 @@ defmodule HxWeb.Gettext do
   @doc """
   Translate error messages for the given field.
   """
-  @spec translate_errors([{String.t(), keyword}], atom) :: list[String.t()]
+  @spec translate_errors([{String.t(), keyword}], atom) :: list(String.t())
   def translate_errors(errors, field) do
     for {^field, {msg, opts}} <- errors do
       translate_error({msg, opts})
